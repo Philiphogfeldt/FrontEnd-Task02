116204302512e7c2995fb75ee8d1829af6570cf1
Philiphogfeldt <philip.hogfeldteberdal@studerande.plushogskolan.se>
2023-02-07 13:36:17 +0100

    x

--- search.js
+++ search.js
@@ -1,29 +1,36 @@
-// Find the elements.
-//https://pixabay.com/api/?key=33414549-51bff4c83ee9ddf1982fd6577&q=yellow+flowers&image_type=photo
+//let url = 'https://pixabay.com/api/?key=33414549-51bff4c83ee9ddf1982fd6577&q=' + searchInput +'&image_type=photo'
 
+// Define the API endpoint URL and API key.
 const apiKey = '33414549-51bff4c83ee9ddf1982fd6577';
+const searchInput = document.querySelector('input[name=searchInput]').value;
 let url = 'https://pixabay.com/api/?key=' + apiKey +'&q=' + searchInput +'&image_type=photo'
+
+// Find the form element and the list element in the HTML.
 let form = document.querySelector('form');
 let list = document.querySelector('#imgList');
 
+// Add an event listener to the form's submit event.
 form.onsubmit = async event => {
-    // Prevent the default "reload page" behavior.
-    event.preventDefault();
+  // Prevent the default form submit behavior.
+  event.preventDefault();
 
-    // Get the values entered by the user.
-    let searchInput = form.searchInput.value;
-  
-    fetch(url);
-    let response = await fetch(url);
-    let json = await response.json();
+  // Fetch the data from the API.
+  let response = await fetch(url);
+  let json = await response.json();
 
-    // Create a list item and set its text content to the city name.
-    let listItem = document.createElement('li');
-    listItem.textContent = searchInput;
-    // Add the list item to the list.
-    list.append(listItem);
+  // Clear the existing list items.
+  list.innerHTML = '';
 
-    // Clear the city name input.
-    form.searchInput.value = '';
+  // Iterate over the returned image data and create list items for each image.
+  json.hits.forEach(img => {
+    let li = document.createElement('li');
+    let image = document.createElement('img');
+    image.src = img.webformatURL;
+    li.appendChild(image);
+    list.appendChild(li);
+  });
+
+  // Clear the search input.
+  form.searchInput.value = '';
 };
 
