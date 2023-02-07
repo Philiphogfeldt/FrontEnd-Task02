61bbbbc24a0ca45a8f9332d98b6dffbe1f6628fd
Philiphogfeldt <philip.hogfeldteberdal@studerande.plushogskolan.se>
2023-02-07 14:14:52 +0100

    x

--- search.js
+++ search.js
@@ -2,31 +2,29 @@
 
 // Define the API endpoint URL and API key.
 const apiKey = '33414549-51bff4c83ee9ddf1982fd6577';
-const searchInput = document.querySelector('input[name=searchInput]').value;
-let url = 'https://pixabay.com/api/?key=' + apiKey +'&q=' + searchInput +'&image_type=photo'
+//let searchInput = document.querySelector('input[name=searchInput]').value;
 
-// Find the form element and the list element in the HTML.
 let form = document.querySelector('form');
 let list = document.querySelector('#imgList');
 
-// Add an event listener to the form's submit event.
 form.onsubmit = async event => {
-  // Prevent the default form submit behavior.
   event.preventDefault();
 
-  // Fetch the data from the API.
+  let searchInput = from.searchInput.value;
+
+  let url = 'https://pixabay.com/api/?key=' + apiKey +'&q=' + searchInput;
+
   let response = await fetch(url);
   let json = await response.json();
-
-  // Clear the existing list items.
-  list.innerHTML = '';
+  
+  let li = document.createElement('li');
+  li.textContent = searchInput + json.hits.tags;
 
   // Iterate over the returned image data and create list items for each image.
   json.hits.forEach(img => {
-    let li = document.createElement('li');
     let image = document.createElement('img');
     image.src = img.webformatURL;
-    li.appendChild(image);
+    list.appendChild(image);
     list.appendChild(li);
   });
 
