e2d679481214da93ef54b4e3198bd68b46e0dc2c
Philip Högfeldt <112342813+Philiphogfeldt@users.noreply.github.com>
2023-02-08 11:38:34 +0100

    photo är med

--- search.js
+++ search.js
@@ -1,8 +1,6 @@
 //let url = 'https://pixabay.com/api/?key=33414549-51bff4c83ee9ddf1982fd6577&q=' + searchInput +'&image_type=photo'
 
-// Define the API endpoint URL and API key.
 const apiKey = '33414549-51bff4c83ee9ddf1982fd6577';
-//let searchInput = document.querySelector('input[name=searchInput]').value;
 
 let form = document.querySelector('form');
 let list = document.querySelector('#imgList');
@@ -17,17 +15,21 @@ form.onsubmit = async event => {
   let response = await fetch(url);
   let json = await response.json();
   
-  let li = document.createElement('li');
+
   //let searchWord = form.SearchInput.value;
 
   // Iterate over the returned image data and create list items for each image.
   json.hits.forEach(img => {
+    let li = document.createElement('li');
     let image = document.createElement('img');
+    let user = document.createElement('p');
     image.src = img.webformatURL;
-    list.appendChild(image);
+    user.textContent = `Photo: ${img.user}`;
     list.appendChild(li);
+    li.appendChild(image);
+    li.appendChild(user);
   });
-
   // Clear the search input.
   form.searchInput.value = '';
+
 };
