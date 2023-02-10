45b2af06b1974da105285f2fa31947f7abd1dce3
Philip Högfeldt <112342813+Philiphogfeldt@users.noreply.github.com>
2023-02-10 09:57:39 +0100

    nextbutton and prev added

--- search.js
+++ search.js
@@ -3,14 +3,17 @@ const apiKey = '33414549-51bff4c83ee9ddf1982fd6577';
 let form = document.querySelector('form');
 let list = document.querySelector('#imgList');
 let url;
+let currentPage = 1;
+let searchInput
 
 form.onsubmit = async event => {
   event.preventDefault();
 
-  let searchInput = form.searchInput.value;
+  searchInput = form.searchInput.value;
 
   //let url = 'https://pixabay.com/api/?key=' + apiKey +'&q=' + searchInput;
-  url = `https://pixabay.com/api/?key=${apiKey}&q=${searchInput}&page=1&per_page=10`;
+  //url = `https://pixabay.com/api/?key=${apiKey}&q=${searchInput}&page=1&per_page=10`;
+  url = `https://pixabay.com/api/?key=${apiKey}&q=${searchInput}&page=${currentPage}&per_page=10`;
 
   let response = await fetch(url);
   let json = await response.json();
@@ -30,22 +33,60 @@ form.onsubmit = async event => {
     li.appendChild(user);
   });
 
-  form.searchInput.value = '';
+  //form.searchInput.value = '';
 
 };
 
-// Initialize variables to keep track of the current page and per_page parameters
-//let page = 1;
-//let per_page = 20;
 
-// Function to retrieve the next set of images
-//function getNextImages() {
-  // Make an API call to retrieve the images for the current page and per_page parameters
-  // ...
+nextButton.onclick = async () => {
 
-  // Update the current page for the next API call
-  //page += 1;
-//}
+  currentPage++;
+  searchInput = form.searchInput.value;
 
-// Attach an event listener to the "Next" button
-//document.getElementById("next-button").addEventListener("click", getNextImages);
+  let url = `https://pixabay.com/api/?key=${apiKey}&q=${searchInput}&page=${currentPage}&per_page=10`;
+
+  let response = await fetch(url);
+  let json = await response.json();
+
+  while (list.firstChild) {
+    list.removeChild(list.firstChild);
+  }
+
+  json.hits.forEach(img => {
+    let li = document.createElement('li');
+    let image = document.createElement('img');
+    let user = document.createElement('photoInfo');
+    image.src = img.webformatURL;
+    user.textContent = `Photo: ${img.user}`;
+    list.appendChild(li);
+    li.appendChild(image);
+    li.appendChild(user);
+  });
+};
+
+previousButton.onclick = async () => {
+
+  currentPage--;
+  searchInput = form.searchInput.value;
+
+  let url = `https://pixabay.com/api/?key=${apiKey}&q=${searchInput}&page=${currentPage}&per_page=10`;
+
+  let response = await fetch(url);
+  let json = await response.json();
+
+  while (list.firstChild) {
+    list.removeChild(list.firstChild);
+  }
+
+  json.hits.forEach(img => {
+    let li = document.createElement('li');
+    let image = document.createElement('img');
+    let user = document.createElement('photoInfo');
+    image.src = img.webformatURL;
+    user.textContent = `Photo: ${img.user}`;
+    list.appendChild(li);
+    li.appendChild(image);
+    li.appendChild(user);
+  });
+};
+
